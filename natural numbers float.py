854b7030a0c23afedaa14ffd5f40ff5139b02284
GangadharNeelam <93145713+GangadharNeelam@users.noreply.github.com>
2021-10-25 23:46:27 +0530

    Add files via upload

---
+++ natural numbers float.py
@@ -0,0 +1,6 @@
+n = float(input("Enter the value of n:"))
+s = n*(n+1)//2
+r = n**2
+print("The sum of first", n, "even natural numbers", s)
+print("The sum of first", n, "odd natural numbers", r)
+print("The difference of s and r =", s-r)
