fae5a536d1d67f4671fcec26cf92cadd92e4d879
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-10-26 16:40:40 +0530

    Update natural numbers input.py

--- natural numbers input.py
+++ natural numbers input.py
@@ -1,6 +1,7 @@
 n = int(input("Enter the value of n:"))
 s = n*(n+1)//2
 r = n**2
+s_s = s-r
 print("The sum of first", n, "even natural numbers", s)
 print("The sum of first", n, "odd natural numbers", r)
-print("The difference of s and r =", s-r)
+print("The difference of s and r =", s_s)
