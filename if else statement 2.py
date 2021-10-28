aac1d10508312fcd7775af263744373e7800563a
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-10-28 17:04:45 +0530

    Add files via upload

---
+++ if else statement 2.py
@@ -0,0 +1,9 @@
+# To check if number is positive ot negative
+
+n = int(input("Enter an Integer:"))    
+if n>0:
+    print(n, "It is a positive integer")
+elif n<0:
+     print(n, "It is a Negative integer")
+else:
+    print(n, "It is a zero")
