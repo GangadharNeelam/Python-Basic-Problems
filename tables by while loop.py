e8ab2a51268c309f1a812fa4d27e3d2711789c3a
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-11-06 16:58:26 +0530

    Add files via upload

---
+++ tables by while loop.py
@@ -0,0 +1,11 @@
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+while a<=b:
+    i = 18
+    while i<=18:
+        print(b, "x", i, "=", b*i)
+        i -= 1
+        if i == 0:
+            break
+    print()
+    b -= 1
