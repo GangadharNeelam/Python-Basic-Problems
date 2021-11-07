a1ca911444db78d847beb8503b9a65f071f3182d
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-11-07 11:01:03 +0530

    Delete km to miles.py

--- km to miles.py
+++
@@ -1,9 +0,0 @@
-# Taking kilometers input from the user
-k = float(input("Enter value in kilometers: "))
-
-# conversion factor
-conv_fac = 0.621371
-
-# calculate miles
-m = k * conv_fac
-print(k , 'kilometers is eqaul to', m, 'miles')
