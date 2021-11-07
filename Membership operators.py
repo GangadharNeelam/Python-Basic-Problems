320193e64835e553adb522cf7a5c9eed9ad725be
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-11-07 10:44:31 +0530

    Add files via upload

---
+++ Membership operators.py
@@ -0,0 +1,12 @@
+#MEMBERSHIP OPEARATORS
+
+# in 
+# not in
+#Example:
+l = input('Enter an alphabet:')
+vowels = 'aeiouAEIOU'
+if l in vowels
+:
+    print(l, 'is an vowel')
+else:
+    print(l, 'is a consonant')
