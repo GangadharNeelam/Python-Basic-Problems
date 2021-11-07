6ccda05c6e056f2e6dfec7d9e59f6768ebca7f66
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-11-07 10:58:25 +0530

    Delete if else statement 2.py

--- if else statement 2.py
+++
@@ -1,9 +0,0 @@
-# To check if number is positive ot negative
-
-n = int(input("Enter an Integer:"))    
-if n>0:
-    print(n, "It is a positive integer")
-elif n<0:
-     print(n, "It is a Negative integer")
-else:
-    print(n, "It is a zero")
