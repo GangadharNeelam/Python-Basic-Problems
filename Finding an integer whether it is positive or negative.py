435f9786ac4039e795f426e92a769060a08e8e32
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-11-07 10:56:16 +0530

    Delete Finding an integer whether it is positive or negative.py

--- Finding an integer whether it is positive or negative.py
+++
@@ -1,8 +0,0 @@
-#Finding an integer whether it is postive or negative
-
-Number = int(input("Enter an Integer:"))
-if Number>0:
-    print("It is a positive integer")
-else:
-    print("It is a Negative integer")
-    
