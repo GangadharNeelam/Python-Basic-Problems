349c3c5677f098ce04bc88c0f5060d93197394e2
GANGADHARA GANNIBABU NEELAM <93145713+GangadharNeelam@users.noreply.github.com>
2021-11-07 09:59:31 +0530

    Delete arithmetical operators.py

--- arithmetical operators.py
+++
@@ -1,9 +0,0 @@
-a = 18
-b = 7
-print("The sum of", a,"and", b,"is", a+b)
-print("The difference between", a,"and", b,"is", a-b)
-print("The product of", a,"and", b,"is", a*b)
-print("The value of 18 to the power 7 is", a**b)
-print("Remainder when", a,"is divided by", b,"is", a%b)
-print("Quetient when", a,"is divided by", b,"is", a/b)
-print("Floor quotient when", a,"is divided by", b,"is", a//b)
